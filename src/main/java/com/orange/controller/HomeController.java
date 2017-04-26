a5f5433f5a79d5ca97e43f67e06c3158677f460b
CiszeMar <marcin.ciszelski@orange.com>
2017-04-26 14:51:01 +0200

    Usuniecie nadmiarowego kodu

--- src/main/java/com/orange/controller/HomeController.java
+++ src/main/java/com/orange/controller/HomeController.java
@@ -2,23 +2,15 @@ package com.orange.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
-//	@RequestMapping("/")
-//	public String hallo() {
-//		return "hello";
-//	}
-	
 	@RequestMapping("/")
-	public ModelAndView hallo() {
-		ModelAndView mav = new ModelAndView("hello");
-		//mav.setViewName("hello2");
-		mav.addObject("name", "Orange");
-		return mav;
+	public String hallo() {
+		return "hello";
 	}
+	
+
 
 }
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,12 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Hello</title>
+<link href="webjars/bootstrap/4.0.0-alpha.6-1/css/bootstrap.css"
+	type="text/css" rel="stylesheet">
 </head>
 <body>
-	<h1>Hello ${name}</h1>
+	<h1>Hello world</h1>
 </body>
 </html>
